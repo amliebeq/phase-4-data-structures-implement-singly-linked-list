6ea4a25600ff1f8f9ee8214bc12ddff5b6952cc3
alex lieberman <alex.lieberman182@gmail.com>
2023-06-14 11:20:11 -0700

    commit tree 30923410bd253f98ae0a2e0e8897f6b6f7027389

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,38 @@
 require_relative './node'
 
 class LinkedList
+    attr_accessor :head
+
+    def initialize
+        @head = nil
+    end
+
+    def prepend (node)
+        self.head = node
+        return
+    end
+
+    def append(node)
+        if head.nil?
+          self.head = node
+          return
+        end
+
+        last_node = head
+        while last_node.next_node
+          last_node = last_node.next_node
+        end
+    
+        last_node.next_node = node
+    end
+
+    def delete_head
+        if head.nil?
+            return @head
+        elsif head.length == 1
+            @head = nil
+            return
+        end
+    end
 
 end
